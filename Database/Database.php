37513950895d1238a632cb1a324546280893a198
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-03-25 00:30:05 +0700

    Add Database Class

---
+++ Database/Database.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Cookie - Library untuk framework kecik, library ini khusus untuk membantu masalah Cookie 
+ *
+ * @author 		Dony Wahyu Isp
+ * @copyright 	2015 Dony Wahyu Isp
+ * @link 		http://github.io/kecik_cookie
+ * @license		MIT
+ * @version 	1.0.1-alpha
+ * @package		Kecik\Database
+ **/
+namespace Kecik;
+
+/**
+ * Database
+ * @package 	Kecik\Database
+ * @author 		Dony Wahyu Isp
+ * @since 		1.0.1-alpha
+ **/
+class Database {
+	private $app;
+
+	private $driver;
+	private $hostname;
+	private $username;
+	private $password;
+	private $dbname;
+
+	private $db=NULL;
+	private $table='';
+	private $data=array();
+
+	/**
+ 	 * __construct
+ 	 * @param Kecik $app
+ 	 **/
+	public function __construct(Kecik $app) {
+		$this->app = $app;
+
+		$config = $app->config;
+		$this->driver = (!empty($config->get('database.driver')))?strtolower($config->get('database.driver')):'';
+		$this->hostname = (!empty($config->get('database.hostname')))?$config->get('database.hostname'):'';
+		$this->username = (!empty($config->get('database.username')))?$config->get('database.username'):'';
+		$this->password = (!empty($config->get('database.password')))?$config->get('database.password'):'';
+		$this->dbname = (!empty($config->get('database.dbname')))?$config->get('database.dbname'):'';
+	}
+
+	public function connect($dbname='', $driver='mysqli', $host='localhost', $username='root', $password='') {
+		$this->driver = (!empty($driver))?strtolower($driver):strtolower($this->driver);
+		$this->hostname = (!empty($host))?$host:$this->hostname;
+		$this->username = (!empty($username))?$username:$this->username;
+		$this->password = (!empty($password))?$password:$this->password;
+		$this->dbname = (!empty($dbname))?$dbname:$this->dbname;
+		
+		if (file_exists(dirname( __FILE__ )."/drivers/".$this->driver.".php")) {
+			$this->db = include_once("drivers/".$this->driver.".php");
+			if (in_array($this->driver, array('sqlite', 'sqlite3')))
+				$this->db->connect($this->dbname);
+			elseif ($this->driver == 'mysqli')
+				$this->db->connect($this->dbname, $this->hostname, $this->username, $this->password);
+		}
+		
+		return $this->db;
+	}
+
+	public function exec($query) {
+		$this->table = '';
+		return $this->db->exec($query);
+	}
+
+	public function fetch($res) {
+		return $this->db->fetch($res);
+	}
+
+	public function affected() {
+		if (in_array($this->driver, array('sqlite', 'sqlite3')) ) {
+			echo 'Fungsi tidak support untuk driver '.$this->driver;
+		} else
+			return $this->db->affected();
+	}
+
+	public function __get($table) {
+        $this->table = $table;
+        return $this;
+    }
+
+	public function insert($data, $table='') {
+		$table = (!empty($this->table))?$this->table:$table;
+		
+		return $this->db->insert($table, $data);
+	}
+
+	public function update($id, $data, $table='') {
+		$table = (!empty($this->table))?$this->table:$table;
+		
+		return $this->db->update($table, $id, $data);
+	}
+
+	public function delete($id, $table='') {
+		$table = (!empty($this->table))?$this->table:$table;
+
+		return $this->db->delete($table, $id);
+	}
+
+	public function find($condition='', $table='') {
+		$table = (!empty($this->table))?$this->table:$table;
+
+		return $this->db->find($table, $condition);
+	}
+
+
+	public function __destruct() {
+		unset($this->db);
+	}
+}
